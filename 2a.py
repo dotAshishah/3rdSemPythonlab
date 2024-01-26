6707af0781a272388f0a0394cc685178028a16ef
Ashish Shah <133593891+dotAshishah@users.noreply.github.com>
2024-01-26 21:47:50 +0530

    Create 2a.py

---
+++ 2a.py
@@ -0,0 +1,17 @@
+# 2a) Defined as a function F as Fn = Fn-1 + Fn-2. Write a Python program that accepts a value 
+# for N (where N >0) as input and pass this value to the function. Display a suitable error 
+# message if the condition for input value is not followed. 
+
+
+def fn(n):
+ if n == 1:
+ return 0
+ elif n == 2:
+ return 1
+ else:
+ return fn(n-1) + fn(n-2)
+num = int(input("Enter a number : "))
+if num > 0:
+ print("fn(", num, ") = ",fn(num) , sep ="")
+else:
+ print("Error in input")
