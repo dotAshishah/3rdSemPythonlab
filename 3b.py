3df2f4d636213e5956281e730d277d0929599360
Ashish Shah <133593891+dotAshishah@users.noreply.github.com>
2024-01-27 08:04:17 +0530

    Create 3b.py

---
+++ 3b.py
@@ -0,0 +1,15 @@
+# 3b) Write a Python program to find the string similarity between two given strings.
+str1 = input("Enter String 1 \n")
+str2 = input("Enter String 2 \n")
+if len(str2) < len(str1):
+ short = len(str2)
+ long = len(str1)
+else:
+ short = len(str1)
+ long = len(str2) 
+matchCnt = 0
+for i in range(short):
+ if str1[i] == str2[i]:
+ matchCnt += 1
+print("Similarity between two said strings:")
+print(matchCnt/long)
